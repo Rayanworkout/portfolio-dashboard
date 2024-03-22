74998bc9fbe9f5696dff05f2d0e836ff63c80775
Rayan <rayan13170@protonmail.com>
2024-03-22 11:02:43 +0100

    added flash message and ability to add tx

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, flash
 
 from helpers import create_plot
 from portfolio import Portfolio
 
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
 
 app = Flask(__name__)
+
+app.secret_key = os.getenv("FLASK_SECRET_KEY")
 
 
 @app.route("/")
 def index():
 
-    # pf = Portfolio(name="main", db_name="portfolio/portfolio.sqlite3")
-    pf = Portfolio(name="main", db_name="fake_data.sqlite3")
+    pf = Portfolio(name="main", db_name="portfolio/portfolio.sqlite3")
 
     data = {
         "total_value": pf.get_total_value(),
@@ -24,6 +30,31 @@ def index():
     # chart_div = create_plot()
 
     return render_template("index-page.html", portfolio=data, holdings=holdings)
+
+
+@app.route("/new", methods=["GET", "POST"])
+def new():
+    if request.method == "POST":
+
+        pf = Portfolio(name="main", db_name="portfolio/portfolio.sqlite3")
+
+        token = request.form["token"]
+        qty = request.form["qty"]
+        price = request.form["price"]
+        fees = request.form["fees"]
+
+        tx = {
+            "qty": qty,
+            "token": token,
+            "cost": int(price) * float(qty),
+            "fees": fees,
+        }
+
+        pf.db.add_transaction(tx)
+        flash("Transaction added successfully!", "success")
+        return redirect("/")
+
+    return render_template("new-tx-page.html")
 
 
 # flask --app app run --debug
